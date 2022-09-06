c85d221a0753767e8f0a59861bea59d106326ce6
sadik <po480373@gmai.com>
2022-09-06 20:09:17 +0600

    random user controller added

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,10 +1,18 @@
 const users = require("../user.json");
 
+//* get all users
 const getAllUsers = (req, res) => {
   const { limit, page } = req.query;
   res.send(users.slice(0, limit));
 };
+//* random user
+const getRandomUser = (req, res) => {
+  const string = JSON.stringify(users);
+  const parsedUser = JSON.parse(string);
 
+  res.send(parsedUser[Math.floor(Math.random() * parsedUser.length)]);
+};
 module.exports = {
   getAllUsers,
+  getRandomUser,
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const userController = require("../controllers/UserController");
 
 const router = express.Router();
-
+//* get all user
 router.route("/all").get(userController.getAllUsers);
+//* random user
+router.route("/random").get(userController.getRandomUser);
 
 module.exports = router;
